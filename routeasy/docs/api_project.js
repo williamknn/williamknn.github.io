e41cca6b0b82a862d25a63cb65437c540a9fd7f0
William Kennedy <will.lindemann@gmail.com>
2016-09-14 17:51:58 -0300

    add routeasy docs

---
+++ routeasy/docs/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "routeasy",
+  "version": "0.9.0",
+  "description": "Routeasy API Documentation",
+  "title": "Routeasy API Documentation",
+  "url": "http://www.routeasy.com.br",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-09-14T20:50:49.312Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
